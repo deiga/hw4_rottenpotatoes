7dc131beeab68277be2aeb23b31e5852862982f8
Timo Sand <timo.sand@kapsi.fi>
2012-03-24 15:59:59 +0200

    Added root path

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -1,9 +1,22 @@
 Given /^the following movies exist:$/ do |table|
+  pre_size = Movie.all.count
   table.hashes.each do |movie|
     Movie.create!(movie)
   end
+  post_size = Movie.all.count
+  post_size.should be pre_size+table.hashes.size
 end
 
 When /^(?:|I )go to (?:the )?(\w+) page for "(.*?)"$/ do |action, page_name|
-  visit edit_movie_path(Movie.find_by_title(page_name).id)
+  movie_id = Movie.find_by_title(page_name).id
+  case action
+  when "edit"
+    visit edit_movie_path(movie_id)
+  when "details"
+    visit movie_path(movie_id)
+  end
+end
+
+Given /^(?:|I )am on (?:the )?(\w+) page for "(.*?)"$/ do |action, page_name|
+  step %Q{I go to the #{action} page for "#{page_name}"}
 end
