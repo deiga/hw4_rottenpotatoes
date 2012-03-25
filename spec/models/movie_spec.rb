53057ca66586879c0e235ae2d9ed1de78400065b
Timo Sand <timo.sand@kapsi.fi>
2012-03-25 16:46:44 +0300

    FIXED: Movie spec be_nil bug

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -8,7 +8,9 @@ describe Movie do
   end 
 
   describe "find with director" do
-    Movie.create!(:title => "Star Wars: A New Hope", :rating => "PG", :release_date => "1977-05-25", :director => "George Lucas")
-    Movie.find_by_director("George Lucas").should_not be_nil
+    it "should find any Movie with a director" do
+      Movie.create!(:title => "Star Wars: A New Hope", :rating => "PG", :release_date => "1977-05-25", :director => "George Lucas")
+      Movie.find_by_director("George Lucas").should_not be_nil
+    end
   end
 end
