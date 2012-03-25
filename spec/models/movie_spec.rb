1883bd35da315f21ed3ec96e19418bb55a0f8d6b
Timo Sand <timo.sand@kapsi.fi>
2012-03-25 16:15:35 +0300

    Added test for director search

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -6,4 +6,9 @@ describe Movie do
       Movie.create!(:title => "Alien", :rating => "R", :release_date => "1979-05-25").should be_valid
     end
   end 
+
+  describe "find with director" do
+    Movie.create!(:title => "Star Wars: A New Hope", :rating => "PG", :release_date => "1977-05-25", :director => "George Lucas")
+    Movie.where(:director => "George Lucas").count.should == 1
+  end
 end
