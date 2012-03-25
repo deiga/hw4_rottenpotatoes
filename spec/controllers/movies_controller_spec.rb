b4bffebdbf524bced78ada3d737887115d78dc55
Timo Sand <timo.sand@kapsi.fi>
2012-03-25 17:19:17 +0300

    RENAMED: find_with_director -> with_same_director

--- spec/controllers/movies_controller_spec.rb
+++ spec/controllers/movies_controller_spec.rb
@@ -1,11 +1,11 @@
 require 'spec_helper'
 
 describe MoviesController do 
-  describe "Find movies movies with same director" do
-    it "should call the model method with correct arguments" do
+  describe "GET find_with_director" do
+    it "should call model with director" do
       movie = Movie.find_by_director("George Lucas")
       Movie.should_receive(:find_by_director).with("George Lucas")
-      get :find_with_director, :id => movie.id
+      get :with_same_director, :id => movie.id
     end
   end
 end
