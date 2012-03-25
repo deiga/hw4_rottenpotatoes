9c1578c3a8001c448c764c6b7f0c0aa35cacf3d3
Timo Sand <timo.sand@kapsi.fi>
2012-03-25 16:47:27 +0300

    FIXED: Spec for movies controller find with director

--- spec/controllers/movies_controller_spec.rb
+++ spec/controllers/movies_controller_spec.rb
@@ -3,8 +3,9 @@ require 'spec_helper'
 describe MoviesController do 
   describe "Find movies movies with same director" do
     it "should call the model method with correct arguments" do
+      movie = Movie.find_by_director("George Lucas")
       Movie.should_receive(:find_by_director).with("George Lucas")
-      get("find_with_director", :director => "George Lucas")
+      get :find_with_director, :id => movie.id
     end
   end
 end
