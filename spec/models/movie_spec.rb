4f8ebd27e9383e074c5b5b4809a0a033deb9bebd
Timo Sand <timo.sand@kapsi.fi>
2012-03-25 17:36:33 +0300

    ADDED: Spec for movie

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -1,6 +1,11 @@
 require 'spec_helper'
 
 describe Movie do
+  describe "all ratings" do
+    it "should display all ratings" do
+      Movie.all_ratings.should == %w{G PG PG-13 NC-17 R}
+    end
+  end
   describe "create Movie" do
     it "should not fail" do
       Movie.create!(:title => "Alien", :rating => "R", :release_date => "1979-05-25").should be_valid
